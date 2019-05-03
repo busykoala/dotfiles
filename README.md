8cc162748f7865ca7bde0fa001e08b917574b697
Matthias Osswald <m@osswald.li>
2019-05-03 10:25:55 +0200

    Major changes excluding vim as separate repo

--- README.md
+++ README.md
@@ -3,47 +3,9 @@
 Clone the repository to the home directory
 
 ```zsh
-cd ~
-git clone git@github.com:busykoala/dotfiles.git
-```
-
-Symlink the applications `.*rc` files to the dotfiles
-
-```zsh
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitignore ~/.gitignore
-ln -s ~/dotfiles/python/.pythonrc ~/.pythonrc
-ln -s ~/dotfiles/python/.pdbrc ~/.pdbrc
-ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc
-```
-
-Add computer specific config
-
-```zsh
-mkdir ~/dotfiles/user_specific
-touch ~/dotfiles/user_specific/.gitconfig_expand
-```
-```zsh
-[user]
-    signingkey = XXXXXXXXXXXX
-```
-
-
-## Configure Vim
-
-```zsh
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-```
-
-Install Plugins with: `:PluginInstall`
-
-If YouCompleteMe does not work yet:
-
-```zsh
-pacman -S cmake
-cd ~.vim/bundle/YouCompleteMe
-./install.py
+git clone git@github.com:busykoala/dotfiles.git ~/dotfiles
+chmod +x ~/dotfiles/setup.sh
+cd ~/dotfiles
+./setup.sh
 ```
 
---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+VIM_REPO_FOLDER=~/vim_config
+VIM_REPO_URL=git@github.com:busykoala/vim_config.git
+if [ ! -d "$VIM_REPO_FOLDER" ] ; then
+	echo "The vim repo gets cloned"
+	git clone $VIM_REPO_URL $VIM_REPO_FOLDER
+else
+	echo "The vim repos exists, it is getting pulled"
+	git -C $VIM_REPO_FOLDER pull
+fi
+
+# Symlink .vimrc
+ln -sf ~/vim_config/.vimrc ~/.vimrc
+
+YCM_REPO_FOLDER=~/.vim/bundle/Vundle.vim
+YCM_REPO_URL=https://github.com/gmarik/Vundle.vim.git
+if [ ! -d "$YCM_REPO_FOLDER" ] ; then
+	echo "The YCM repo gets cloned"
+	git clone $YCM_REPO_URL $YCM_REPO_FOLDER
+else
+	echo "The YCM repos exists, it is getting pulled"
+	git -C $YCM_REPO_FOLDER pull
+fi
+
+# YCM Configuration information
+echo "Please see https://github.com/Valloric/YouCompleteMe"
+echo "and configure accordingly."
+echo ""
+echo "Also install astyle and for R the package lintr"
+echo ""
+echo "See https://github.com/busykoala/vim_config for more info"
+
+# Symlink dotfiles
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/python/.pythonrc ~/.pythonrc
+ln -sf ~/dotfiles/python/.pdbrc ~/.pdbrc
+ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
+
