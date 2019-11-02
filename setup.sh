998a9aefb86ba5986eab76a779dbca730f5c8c53
Matthias Osswald <m@osswald.li>
2019-11-02 02:12:41 +0100

    Make setup script an update script as well.

--- setup.sh
+++ setup.sh
@@ -2,22 +2,30 @@
 
 # install command-line tools for Xcode
 xcode-select --install > /dev/null 2>&1
+# allow to open in terminal from unknown
+# sudo spctl --master-disable
 
-# install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# install from Brewfile
+if hash brew 2>/dev/null; then
+    echo "--> Brew is installed. Not doing anything."
+else
+    echo "--> Brew is getting installed."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "--> Dependencies from Brewfile are getting installed."
 brew bundle install
 
 VIM_REPO_FOLDER=~/vim_config
 VIM_REPO_URL=git@github.com:busykoala/vim_config.git
 if [ ! -d "$VIM_REPO_FOLDER" ] ; then
-    echo "The vim repo gets cloned"
+    echo "--> The vim repo gets cloned."
     git clone $VIM_REPO_URL $VIM_REPO_FOLDER
 else
-    echo "The vim repos exists, it is getting pulled"
+    echo "--> The vim repos exists, it is getting pulled."
     git -C $VIM_REPO_FOLDER pull
 fi
 
+echo "--> Symlink and (re-)setup environment."
 # Symlink .vimrc
 ln -sf ~/vim_config/.vimrc ~/.vimrc
 ln -sf ~/vim_config/.editorconfig ~/.editorconfig
@@ -42,3 +50,9 @@ ln -sf ~/dotfiles/python/.pdbrc ~/.pdbrc
 ln -sf ~/dotfiles/python/.pythonrc ~/.pythonrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.buildout_default.cfg ~/.buildout/.default.cfg
+
+echo "--> Update & upgrade brew now."
+brew update
+brew upgrade
+brew cask upgrade
+mas upgrade
